ab06f25162da77b94fa6224e48b037dba9e59673
nando-castro <fernando.castro10.master@gmail.com>
2022-05-21 12:08:21 -0300

    retorno filmes api e loading

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Footer from "./Components/footer/Footer";
+import Main from "./Components/home/Main";
 import Topo from "./Components/home/Topo";
 
 export default function App() {
 
     return (
         <>
-            <Topo />
-            <Footer />
+        <Topo />
         </>
     );
 }
--- src/Components/home/Main.js
+++ src/Components/home/Main.js
@@ -1,22 +1,54 @@
-import { useState, useEffect } from 'react';
+import React from 'react';
 import axios from 'axios';
-import api from '../../services/api';
+import img from '../../Assets/img/loading.gif';
 
-export default function Main() {
-
-    const [filmes, setFilmes] = useState("");
-
-    useEffect(() => {
-        api
-            .get("/movies")
-            .then((response) => setFilmes(response.data))
-            .catch((err) => {
-                console.error("ops! ocorreu um erro" + err);
-            });
-    }, []);
+function Filme({ source }) {
     return (
-        <div className='flex-center main'>
-            <p>Title : {filmes.title}</p>
+        <div className='flex-center filme'>
+            <img src={source} />
         </div>
     );
 }
+
+function Loading (){
+    return (
+        <>
+        <img src={img} alt='Carregando' />
+        </>
+    );
+}
+
+export default function Main() {
+
+    const [filmes, setFilmes] = React.useState([]);
+
+    React.useEffect(() => {
+
+        const promise = axios.get("https://mock-api.driven.com.br/api/v5/cineflex/movies");
+
+        promise.then(response => {
+
+            setFilmes([...response.data])
+        });
+
+    }, []);
+    console.log(filmes);
+    return (
+        <div className='flex-center main'>
+            {
+                filmes.length === 0 ? <Loading /> :
+                    filmes.map(value =>
+                        <Filme
+                            source={value.posterURL}
+                        />)}
+        </div>
+    );
+}
+
+
+/*
+.then((response) => setFilmes(response.data))
+            .catch((err) => {
+                console.error("ops! ocorreu um erro" + err);
+            });
+*/

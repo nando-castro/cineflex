abf15701111e785456d4f87c62a14d9b8d6c6caf
nando-castro <fernando.castro10.master@gmail.com>
2022-05-22 16:36:58 -0300

    seats

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from 'react';
-
+import "./Assets/css/reset.css"
+import "./Assets/css/style.css"
 import Main from "./Components/home/Main"
 import Header from "./Components/home/Header";
 import SessionsTimes from "./Components/session/SessionsTimes";
-
-import "./Assets/css/reset.css"
-import "./Assets/css/style.css"
+import Seats from "./Components/seat/Seats";
 
 export default function App() {
 
-    const [returnButton, setReturnButton] = useState("button-return")
+    const [orderData, setOrderData] = useState("");
+    const [returnButton, setReturnButton] = useState("button-return");
 
     return (
         <BrowserRouter>
@@ -18,6 +18,7 @@ export default function App() {
             <Routes>
                 <Route path="/" element={<Main setReturnButton={setReturnButton} />}></Route>
                 <Route path="/filme/:idMovie" element={<SessionsTimes />}></Route>
+                <Route path="/assentos/:idSessao" element={<Seats orderData={orderData} setOrderData={setOrderData} setReturnButton={setReturnButton}/>}></Route>
             </Routes>
         </BrowserRouter>
     );
